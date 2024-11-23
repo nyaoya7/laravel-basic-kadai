1657720c209bdd047a01fc705be0cbabb3b127b4
nyaoya7 <n.yasugi@gmail.com>
2024-11-23 16:30:13 +0900

    課題データの作成とバリデーション対応

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
+use App\Http\Requests\PostStoreRequest;
 
 class PostController extends Controller
 {
@@ -19,5 +20,18 @@ class PostController extends Controller
         $post = Post::find($id);
 
         return view ('posts.show', compact('post'));
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(PostStoreRequest $request) {
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+        
+        return redirect('posts');
     }
 }
